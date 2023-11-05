d9bc7f90d8cdda75515f2a5e42ac5a85209c6279
Alejandro Nieto <alejandronietoalarconpersonal@gmail.com>
2023-11-05 20:52:00 +0100

    Agregar archivos de proyecto.

---
+++ Tuto/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Tuto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(104, 61);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(233, 562);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1329, 164);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(240, 148);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "LOAD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnCargar_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(454, 61);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(353, 562);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1639, 920);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ Tuto/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Leap;
+
+namespace Tuto
+{
+
+    public partial class Form1 : Form, ILeapEventDelegate
+    {
+
+        private Controller controller;
+        private LeapEventListener listener;
+        private long currentTime;
+        private long previousTime;
+        private long timeChange;
+
+        public Form1()
+        {
+            InitializeComponent();
+            loadSPK();
+            this.controller = new Controller();
+            this.listener = new LeapEventListener(this);
+            controller.AddListener(listener);
+        }
+
+        delegate void LeapEventDelegate(string EventName);
+        public void LeapEventNotification(string EventName)
+        {
+            if (!this.InvokeRequired)
+            {
+                switch (EventName)
+                {
+                    case "onInit":
+                        //MessageBox.Show("Initialize");
+                        break;
+                    case "onConnect":
+                        //MessageBox.Show("Connected");
+                        break;
+                    case "onFrame":
+
+                        //MessageBox.Show("Frame");
+                        //SetCursorPos(1, 2);
+                        movingMouse(this.controller.Frame(), this.controller);
+                        break;
+                }
+            }
+            else
+            {
+                BeginInvoke(new LeapEventDelegate(LeapEventNotification), new object[] { EventName });
+            }
+        }
+
+        private void movingMouse(Leap.Frame frame, Controller controlador)
+        {
+
+            currentTime = frame.Timestamp;
+            timeChange = currentTime - previousTime;
+
+            if (timeChange > 10000)
+            {
+                if (frame.Hands.Count > 0)
+                {
+                    Finger finger = frame.Fingers[0];
+                    // Get the closest screen intercepting a ray projecting from the finger
+                    Leap.Screen screen  = controlador.LocatedScreens.ClosestScreenHit(finger);
+
+                    if (screen != null && screen.IsValid)
+                    {
+                        // Get the velocity of the finger tip
+                        var tipVelocity = (int)finger.TipVelocity.Magnitude;
+
+                        // Use tipVelocity to reduce jitters when attempting to hold
+                        // the cursor steady
+                        if (tipVelocity > 70)
+                        {
+                            var xScreenIntersect = screen.Intersect(finger, true).x;
+                            var yScreenIntersect = screen.Intersect(finger, true).y;
+
+                            if (xScreenIntersect.ToString() != "NaN")
+                            {
+                                var x = (int)(xScreenIntersect * screen.WidthPixels);
+                                var y = (int)(screen.HeightPixels - (yScreenIntersect * screen.HeightPixels));
+
+                                Console.WriteLine("Screen intersect X: " + xScreenIntersect.ToString());
+                                Console.WriteLine("Screen intersect Y: " + yScreenIntersect.ToString());
+                                Console.WriteLine("Width pixels: " + screen.WidthPixels.ToString());
+                                Console.WriteLine("Height pixels: " + screen.HeightPixels.ToString());
+
+                                Console.WriteLine("\n");
+
+                                Console.WriteLine("x: " + x.ToString());
+                                Console.WriteLine("y: " + y.ToString());
+
+                                Console.WriteLine("\n");
+
+                                Console.WriteLine("Tip velocity: " + tipVelocity.ToString());
+
+                                // Move the cursor
+                                MouseCursor.MoveCursor(x, y);
+
+                                Console.WriteLine("\n" + new String('=', 40) + "\n");
+                            }
+
+                        }
+                    }
+                }
+            }
+
+            foreach (Finger finger in frame.Fingers)
+            {
+                richTextBox2.AppendText("Finger id " + finger.Id + Environment.NewLine);
+
+            }
+        }
+
+        private void loadSPK()
+        {
+            string rutaImagen = @"C:\Users\Alejandro\Documents\4to\NPI\Leap Motion\UNI.png"; // Reemplaza "nombreDeTuArchivo.jpg" con el nombre real de tu archivo de imagen.
+            //string rutaImagen = @"C:\Users\Alejandro\Documents\4to\NPI\Leap Motion\MODELOETSIIT.skp"; // Reemplaza "nombreDeTuArchivo.jpg" con el nombre real de tu archivo de imagen.
+
+            try
+            {
+                // Carga la imagen desde la ruta especificada
+                Image imagen = Image.FromFile(rutaImagen);
+
+                // Asigna la imagen al PictureBox pictureBox2
+                pictureBox2.Image = imagen;
+
+                MessageBox.Show("Imagen cargada correctamente.");
+            }
+            catch (Exception ex)
+            {
+                // Manejo de errores
+                MessageBox.Show("Error al cargar la imagen: " + ex.Message);
+            }
+        }
+
+        private void btnCargar_Click(object sender, MouseEventArgs e)
+        {
+            
+            
+            
+        }
+    }
+
+    class MouseCursor
+    {
+        [DllImport("user32.dll")]
+        private static extern bool SetCursorPos(int x, int y);
+
+        public static void MoveCursor(int x, int y)
+        {
+            SetCursorPos(x, y);
+        }
+
+    }
+    public interface ILeapEventDelegate
+    {
+        void LeapEventNotification(string EventName);
+    }
+
+    public class LeapEventListener : Listener
+    {
+        ILeapEventDelegate eventDelegate;
+
+        public LeapEventListener(ILeapEventDelegate delegateObject)
+        {
+            this.eventDelegate = delegateObject;
+        }
+        public override void OnInit(Controller controller)
+        {
+            this.eventDelegate.LeapEventNotification("onInit");
+        }
+        public override void OnConnect(Controller controller)
+        {
+            this.eventDelegate.LeapEventNotification("onConnect");
+        }
+        public override void OnFrame(Controller cntrlr)
+        {
+            this.eventDelegate.LeapEventNotification("onFrame");        
+        }
+        public override void OnExit(Controller controller)
+        {
+            this.eventDelegate.LeapEventNotification("onExit");
+        }
+        public override void OnDisconnect(Controller controller)
+        {
+            this.eventDelegate.LeapEventNotification("onDisconnect");
+        }
+    }
+}
+
+

c4b92897910020b7ec6eb5195705c94b3f01f05d
Daniel Jimenez <danijs2002@gmail.com>
2023-11-26 12:11:12 +0100

    Qr changes2

--- Tuto/Form2.Designer.cs
+++ Tuto/Form2.Designer.cs
@@ -52,9 +52,10 @@
             // 
             // botonStart
             // 
-            this.botonStart.Location = new System.Drawing.Point(294, 876);
+            this.botonStart.Location = new System.Drawing.Point(220, 712);
+            this.botonStart.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.botonStart.Name = "botonStart";
-            this.botonStart.Size = new System.Drawing.Size(75, 23);
+            this.botonStart.Size = new System.Drawing.Size(56, 19);
             this.botonStart.TabIndex = 0;
             this.botonStart.Text = "&Start";
             this.botonStart.UseVisualStyleBackColor = true;
@@ -62,18 +63,20 @@
             // 
             // txtQRcode
             // 
-            this.txtQRcode.Location = new System.Drawing.Point(399, 917);
+            this.txtQRcode.Location = new System.Drawing.Point(299, 745);
+            this.txtQRcode.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtQRcode.Multiline = true;
             this.txtQRcode.Name = "txtQRcode";
-            this.txtQRcode.Size = new System.Drawing.Size(91, 77);
+            this.txtQRcode.Size = new System.Drawing.Size(69, 63);
             this.txtQRcode.TabIndex = 3;
             // 
             // pictureBox1
             // 
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pictureBox1.Location = new System.Drawing.Point(294, 917);
+            this.pictureBox1.Location = new System.Drawing.Point(220, 745);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(75, 70);
+            this.pictureBox1.Size = new System.Drawing.Size(57, 58);
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
             // 
@@ -84,9 +87,10 @@
             // 
             // buttonExit
             // 
-            this.buttonExit.Location = new System.Drawing.Point(128, 1012);
+            this.buttonExit.Location = new System.Drawing.Point(96, 822);
+            this.buttonExit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonExit.Name = "buttonExit";
-            this.buttonExit.Size = new System.Drawing.Size(75, 23);
+            this.buttonExit.Size = new System.Drawing.Size(56, 19);
             this.buttonExit.TabIndex = 5;
             this.buttonExit.Text = "Exit";
             this.buttonExit.UseVisualStyleBackColor = true;
@@ -97,27 +101,31 @@
             this.hourLabelTable.ColumnCount = 1;
             this.hourLabelTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.hourLabelTable.Controls.Add(this.hourLabel, 0, 0);
-            this.hourLabelTable.Location = new System.Drawing.Point(560, 1);
+            this.hourLabelTable.Location = new System.Drawing.Point(420, 1);
+            this.hourLabelTable.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.hourLabelTable.Name = "hourLabelTable";
             this.hourLabelTable.RowCount = 1;
             this.hourLabelTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.hourLabelTable.Size = new System.Drawing.Size(127, 49);
+            this.hourLabelTable.Size = new System.Drawing.Size(95, 40);
             this.hourLabelTable.TabIndex = 6;
             // 
             // hourLabel
             // 
             this.hourLabel.AutoSize = true;
-            this.hourLabel.Location = new System.Drawing.Point(3, 0);
+            this.hourLabel.Font = new System.Drawing.Font("Yu Gothic UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hourLabel.Location = new System.Drawing.Point(2, 0);
+            this.hourLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.hourLabel.Name = "hourLabel";
-            this.hourLabel.Size = new System.Drawing.Size(44, 16);
+            this.hourLabel.Size = new System.Drawing.Size(37, 15);
             this.hourLabel.TabIndex = 0;
             this.hourLabel.Text = "label1";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(241, 831);
+            this.textBox2.Location = new System.Drawing.Point(181, 675);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(185, 22);
+            this.textBox2.Size = new System.Drawing.Size(140, 20);
             this.textBox2.TabIndex = 8;
             this.textBox2.Text = "Escanea tu código QR aquí";
             this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -128,9 +136,10 @@
             this.comboBox1.Items.AddRange(new object[] {
             "ES",
             "EN"});
-            this.comboBox1.Location = new System.Drawing.Point(480, 1012);
+            this.comboBox1.Location = new System.Drawing.Point(360, 822);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(47, 24);
+            this.comboBox1.Size = new System.Drawing.Size(36, 21);
             this.comboBox1.TabIndex = 9;
             this.comboBox1.Text = "ES";
             // 
@@ -139,54 +148,59 @@
             this.dayLabelTable.ColumnCount = 1;
             this.dayLabelTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.dayLabelTable.Controls.Add(this.dayLabel, 0, 0);
-            this.dayLabelTable.Location = new System.Drawing.Point(443, 1);
+            this.dayLabelTable.Location = new System.Drawing.Point(332, 1);
+            this.dayLabelTable.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dayLabelTable.Name = "dayLabelTable";
             this.dayLabelTable.RowCount = 1;
             this.dayLabelTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.dayLabelTable.Size = new System.Drawing.Size(120, 49);
+            this.dayLabelTable.Size = new System.Drawing.Size(90, 40);
             this.dayLabelTable.TabIndex = 10;
             // 
             // dayLabel
             // 
             this.dayLabel.AutoSize = true;
-            this.dayLabel.Location = new System.Drawing.Point(3, 0);
+            this.dayLabel.Font = new System.Drawing.Font("Yu Gothic UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dayLabel.Location = new System.Drawing.Point(2, 0);
+            this.dayLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.dayLabel.Name = "dayLabel";
-            this.dayLabel.Size = new System.Drawing.Size(44, 16);
+            this.dayLabel.Size = new System.Drawing.Size(37, 15);
             this.dayLabel.TabIndex = 0;
             this.dayLabel.Text = "label1";
             // 
             // dispositivo
             // 
             this.dispositivo.FormattingEnabled = true;
-            this.dispositivo.Location = new System.Drawing.Point(246, 876);
+            this.dispositivo.Location = new System.Drawing.Point(184, 712);
+            this.dispositivo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dispositivo.Name = "dispositivo";
-            this.dispositivo.Size = new System.Drawing.Size(42, 24);
+            this.dispositivo.Size = new System.Drawing.Size(32, 21);
             this.dispositivo.TabIndex = 11;
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Snow;
             this.panel1.Controls.Add(this.pictureBox2);
-            this.panel1.Location = new System.Drawing.Point(72, 202);
+            this.panel1.Location = new System.Drawing.Point(54, 164);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(557, 466);
+            this.panel1.Size = new System.Drawing.Size(418, 379);
             this.panel1.TabIndex = 12;
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(220, 37);
+            this.pictureBox2.Location = new System.Drawing.Point(165, 30);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(132, 216);
+            this.pictureBox2.Size = new System.Drawing.Size(99, 176);
             this.pictureBox2.TabIndex = 0;
             this.pictureBox2.TabStop = false;
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Teal;
-            this.BackgroundImage = global::Tuto.Properties.Resources.Pantalla_Background;
-            this.ClientSize = new System.Drawing.Size(699, 1055);
+            this.ClientSize = new System.Drawing.Size(524, 857);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.dispositivo);
             this.Controls.Add(this.dayLabelTable);
@@ -197,6 +211,7 @@
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.txtQRcode);
             this.Controls.Add(this.botonStart);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form2";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "QR Code";

27da00ca9b89a079fefebfd2641e0821ee70dbb1
danijs4 <danijs2002@gmail.com>
2023-11-28 17:22:54 +0100

    QR

--- Tuto/Form2.Designer.cs
+++ Tuto/Form2.Designer.cs
@@ -35,7 +35,6 @@
             this.buttonExit = new System.Windows.Forms.Button();
             this.hourLabelTable = new System.Windows.Forms.TableLayoutPanel();
             this.hourLabel = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.dispositivo = new System.Windows.Forms.ComboBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
@@ -106,21 +105,6 @@
             this.hourLabel.Text = "label1";
             this.hourLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // comboBox1
-            // 
-            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.comboBox1.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "ES",
-            "EN"});
-            this.comboBox1.Location = new System.Drawing.Point(620, 998);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(47, 29);
-            this.comboBox1.TabIndex = 9;
-            this.comboBox1.Text = "ES";
-            // 
             // dispositivo
             // 
             this.dispositivo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
@@ -151,11 +135,11 @@
             // 
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(177, 289);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(223, 256);
+            this.label1.Size = new System.Drawing.Size(223, 267);
             this.label1.TabIndex = 1;
             this.label1.Text = "To log in you need your personal UGR QR code. \r\nYou can find it in your UGR App p" +
     "rofile!";
@@ -241,7 +225,6 @@
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.dispositivo);
             this.Controls.Add(this.dayLabelTable);
-            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.hourLabelTable);
             this.Controls.Add(this.buttonExit);
             this.Controls.Add(this.pictureBox1);
@@ -269,7 +252,6 @@
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button buttonExit;
         private System.Windows.Forms.TableLayoutPanel hourLabelTable;
-        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.ComboBox dispositivo;
         private System.Windows.Forms.Label hourLabel;
         private System.Windows.Forms.Panel panel1;

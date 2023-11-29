cc6100790f7302075322a5623b92f4a1d4ef67db
danijs4 <danijs2002@gmail.com>
2023-11-29 17:46:28 +0100

    Some updates

--- Tuto/Form2.Designer.cs
+++ Tuto/Form2.Designer.cs
@@ -87,10 +87,11 @@
             // 
             // dispositivo
             // 
-            this.dispositivo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.dispositivo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dispositivo.BackColor = System.Drawing.Color.Snow;
             this.dispositivo.FormattingEnabled = true;
-            this.dispositivo.Location = new System.Drawing.Point(25, 857);
+            this.dispositivo.Location = new System.Drawing.Point(326, 891);
             this.dispositivo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dispositivo.Name = "dispositivo";
             this.dispositivo.Size = new System.Drawing.Size(60, 24);
@@ -104,7 +105,7 @@
             this.panel1.Location = new System.Drawing.Point(3, 2);
             this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(515, 318);
+            this.panel1.Size = new System.Drawing.Size(515, 317);
             this.panel1.TabIndex = 12;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
@@ -116,7 +117,7 @@
             this.pictureBox2.Location = new System.Drawing.Point(0, 0);
             this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(515, 318);
+            this.pictureBox2.Size = new System.Drawing.Size(515, 317);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 0;
             this.pictureBox2.TabStop = false;
@@ -131,7 +132,7 @@
             this.label3.Location = new System.Drawing.Point(4, 0);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(513, 90);
+            this.label3.Size = new System.Drawing.Size(513, 91);
             this.label3.TabIndex = 13;
             this.label3.Text = "Scan your QR code here";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -141,7 +142,7 @@
             this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pictureBox1.Location = new System.Drawing.Point(61, 92);
+            this.pictureBox1.Location = new System.Drawing.Point(61, 93);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(398, 219);
@@ -174,7 +175,7 @@
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 74.70998F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.29002F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(521, 431);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(521, 430);
             this.tableLayoutPanel1.TabIndex = 14;
             // 
             // label1
@@ -182,7 +183,7 @@
             this.label1.BackColor = System.Drawing.Color.Snow;
             this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(5, 322);
+            this.label1.Location = new System.Drawing.Point(5, 321);
             this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(511, 109);
@@ -201,12 +202,12 @@
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel2.Controls.Add(this.label3, 0, 0);
             this.tableLayoutPanel2.Controls.Add(this.pictureBox1, 0, 1);
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 440);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 439);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 2;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 29.01961F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70.98039F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(521, 313);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(521, 314);
             this.tableLayoutPanel2.TabIndex = 15;
             // 
             // tableLayoutPanel3
@@ -284,10 +285,12 @@
             this.Controls.Add(this.dispositivo);
             this.Controls.Add(this.txtQRcode);
             this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form2";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "QR Code";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form2_FormClosing);
             this.Load += new System.EventHandler(this.Form2_Load);
             this.panel1.ResumeLayout(false);
